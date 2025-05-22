6569fddee70594aa14ee74646b18bda8881c6a20
Deepak-10439 <sdeepak10439@gmail.com>
2025-05-22 16:05:19 +0530

    Created Home Screen UI and Bottom Navigation Bar

---
+++ app/src/main/java/com/example/whatsapp/presentation/homescreen/ChatDesignModel.kt
@@ -0,0 +1,10 @@
+package com.example.whatsapp.presentation.homescreen
+
+import android.media.Image
+
+data class ChatDesignModel(
+    val image: Int,
+    val time: String,
+    val name: String,
+    val message: String
+)
